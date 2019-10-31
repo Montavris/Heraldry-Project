741523ca6d728a08e92fef086271f319a8d5f446
Montavris <mtavendale1@gmail.com>
2019-10-31 16:01:30 +0000

    Created methods

--- src/Heraldry.java
+++ src/Heraldry.java
@@ -1,4 +1,40 @@
+import java.util.*;
 
 public class Heraldry {
+	public static void main(String[] args) {
+		Heraldry h = new Heraldry();
+		h.CalculateHeraldry();
+	}
 
+	public String CalculateHeraldry() {
+
+		return "";
+	}
+
+	public String FindShield() {
+
+		return "";
+	}
+
+	public String FindTinctures() {
+
+		return "";
+	}
+	
+	public String FindFieldVariations() {
+
+		return "";
+	}
+	public String FindFieldDivisons() {
+
+		return "";
+	}
+	public String FindOrdinaries() {
+
+		return "";
+	}
+	public String FindCharges() {
+
+		return "";
+	}
 }
